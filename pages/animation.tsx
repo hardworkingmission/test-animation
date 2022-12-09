3a784665330ab8e57814b349ee8b2b6531c3b4e5
hardworkingmission <shakil20131101041@gmail.com>
2022-12-09 15:27:30 +0600

    first commit

---
+++ pages/animation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Animation = () => {
+  return (
+    <div className='flex flex-row justify-center p-6 bg-black'>
+      <div className="flex flex-row gap-4">
+        <div className='rainbow'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
